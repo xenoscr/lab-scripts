55780a45c9be000a88bfb9a74b63bdf18da49047
xenoscr <xenos@xenos-1.net>
2021-12-24 09:05:33 -0500

    Adding new configs

--- windows-10-enterprise-eval/virtualbox-automation.bash
+++ windows-10-enterprise-eval/virtualbox-automation.bash
@@ -4,44 +4,76 @@
 VBFolder="/storage/VirtualBox/"
 
 # Windows 10 ISO file path
-isoPath=/storage/ISO/Windows_10/18363.418.191007-0143.19h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso
+isoPath=/storage/ISO/Windows_10
 
-# Set the VM name
-VM="Win10-autoinstall"
+# ISO download URL
+isoURL="https://software-download.microsoft.com/download/sg/19043.928.210409-1212.21h1_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
 
-# Create a new VM of type Windows 10 x64
-VBoxManage createvm --name $VM --ostype "Windows10_64" --register
+# Check to see if a name was supplied
+if [[ $# -lt 1 ]]; then
+    echo "You must supply a hostname.\n\nUsage: $0 <hostname>"
+    exit 1
+fi
 
-# Create a new Disk
-VBoxManage createhd --filename $VBFolder/$VM/$VM.vdi --size 50000
+if [[ `VBoxManage list vms | grep -ie "^\"$VM\"\s" | wc -l` -eq 0 ]]; then
+    isoName=`basename $isoURL`
+    
+    isoFullPath=$isoPath/$isoName
+    
+    if [[ ! -f "$isoFullPath" ]]; then
+        # Download the ISO
+        wget $isoURL --directory-prefix $isoPath
+    fi
+    
+    if [[ -f "$isoFullPath" ]]; then
 
-# Create the SATA Controller
-VBoxManage storagectl $VM --name "SATA Controller" --add sata --controller IntelAHCI
+        # Set the VM name
+        VM=$1
 
-# Attach the drive
-VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VBFolder/$VM/$VM.vdi
+        # Create a new VM of type Windows 10 x64
+        VBoxManage createvm --name $VM --ostype "Windows10_64" --register
 
-# Create an IDE controller for the CD-ROM
-VBoxManage storagectl $VM --name "IDE Controller" --add ide
+        # Create a new Disk
+        VBoxManage createhd --filename $VBFolder/$VM/$VM.vdi --size 100000
 
-# Mount the OS ISO file
-VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "$isoPath"
+        # Create the SATA Controller
+        VBoxManage storagectl $VM --name "SATA Controller" --add sata --controller IntelAHCI
 
-# Turn IOAPIC On
-VBoxManage modifyvm $VM --ioapic on
+        # Attach the drive
+        VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VBFolder/$VM/$VM.vdi
 
-# Set boot order
-VBoxManage modifyvm $VM --boot1 dvd --boot2 disk --boot3 none --boot4 none
+        # Create an IDE controller for the CD-ROM
+        VBoxManage storagectl $VM --name "IDE Controller" --add ide
 
-# Set RAM and VRAM
-VBoxManage modifyvm $VM --memory 4096 --vram 128
+        # Mount the OS ISO file
+        VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "$isoFullPath"
 
-# Unattended install settings
-# Key from http://technet.microsoft.com/en-us/library/jj612867.aspx
-VBoxManage unattended install $VM --iso=/storage/ISO/Windows_10/18363.418.191007-0143.19h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso --user=admin-user --full-user-name="admin-user" --password password --install-additions --time-zone=UTC --key="NPPR9-FWDCX-D2C8J-H872K-2YT43" --country=US --script-template="./Autounattend.xml"
+        # Turn IOAPIC On
+        VBoxManage modifyvm $VM --ioapic on
 
-# Add a network share to the scripts
-VBoxManage sharedfolder add $VM -name "scripts" -hostpath "$PWD/scripts"
+        # Set boot order
+        VBoxManage modifyvm $VM --boot1 dvd --boot2 disk --boot3 none --boot4 none
 
-# VBoxManage startvm $VM --type headless
-VBoxManage startvm $VM
+        # Change the video card
+        VBoxManage modifyvm $VM --graphicscontroller vboxsvga
+
+        # Set RAM and VRAM
+        VBoxManage modifyvm $VM --memory 4096 --vram 128
+
+        # Unattended install settings
+        # Key from http://technet.microsoft.com/en-us/library/jj612867.aspx
+        VBoxManage unattended install $VM --iso="$isoFullPath" --user=admin-user --full-user-name="admin-user" --password password --install-additions --time-zone=UTC --key="NPPR9-FWDCX-D2C8J-H872K-2YT43" --country=US --script-template="./Autounattend.xml"
+
+        # Add a network share to the scripts
+        VBoxManage sharedfolder add $VM -name "scripts" -hostpath "$PWD/scripts"
+
+        # VBoxManage startvm $VM --type headless
+        VBoxManage startvm $VM
+    else
+        echo "The ISO was not found, Exiting!"
+        exit 1
+    fi
+else
+    echo "A VM with the supplied name already exists. Select a new name and try again. Exiting!"
+    exit 1
+fi
---
+++ windows-10-enterprise-eval/virtualbox-automationi-v2.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# VirtialBox folder, change as needed
+VBFolder="/storage/VirtualBox/"
+
+# Windows 10 ISO file path
+isoPath=/storage/ISO/Windows_10/mu_windows_10_language_pack_version_2004_x86_arm64_x64_dvd_7729a9da.iso
+
+# Set the VM name
+VM="Win10-autoinstall"
+
+# Create a new VM of type Windows 10 x64
+VBoxManage createvm --name $VM --ostype "Windows10_64" --register
+
+# Create a new Disk
+VBoxManage createhd --filename $VBFolder/$VM/$VM.vdi --size 100000
+
+# Create the SATA Controller
+VBoxManage storagectl $VM --name "SATA Controller" --add sata --controller IntelAHCI
+
+# Attach the drive
+VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VBFolder/$VM/$VM.vdi
+
+# Create an IDE controller for the CD-ROM
+VBoxManage storagectl $VM --name "IDE Controller" --add ide
+
+# Mount the OS ISO file
+VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "$isoPath"
+
+# Turn IOAPIC On
+VBoxManage modifyvm $VM --ioapic on
+
+# Set boot order
+VBoxManage modifyvm $VM --boot1 dvd --boot2 disk --boot3 none --boot4 none
+
+# Change the video card
+VBoxManage modifyvm $VM --graphicscontroller vboxsvga
+
+# Set RAM and VRAM
+VBoxManage modifyvm $VM --memory 4096 --vram 128
+
+# Unattended install settings
+# Key from http://technet.microsoft.com/en-us/library/jj612867.aspx
+VBoxManage unattended install $VM --iso=/storage/ISO/Windows_10/18363.418.191007-0143.19h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso --user=admin-user --full-user-name="admin-user" --password password --install-additions --time-zone=UTC --key="NPPR9-FWDCX-D2C8J-H872K-2YT43" --country=US --script-template="./Autounattend.xml"
+
+# Add a network share to the scripts
+VBoxManage sharedfolder add $VM -name "scripts" -hostpath "$PWD/scripts"
+
+# VBoxManage startvm $VM --type headless
+VBoxManage startvm $VM
